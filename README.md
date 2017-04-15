958a980e93bb9b25e70be0c00030c6c2181a3918
mduniphin <maxx.duniphin@gmail.com>
2017-04-15 04:58:54 -0500

    prints number of emails

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # 2017-okcoders-week3-HW
+
+
+enron.json
+
+
+1. how many emails are there? - done
+
+2. what is the earliest email (in terms of date)
+
+3. what is the latest email?
+
+4. do any emails contain the word "money" in their text?
+
+5. what sender sent the most emails? (edited)
--- index.js
+++ index.js
@@ -8,9 +8,13 @@ mc.connect(url, function(err, db){
 	if(err){
 		console.log("unable to access database on " + url);
 	}
-	var posts = db.collection('enron');
-	posts.find({}).toArray(function(err,docs){
-		console.log(docs);
+	var emails = db.collection('enron');
+	emails.count(function(err, count){
+		console.log("Number of emails in collection: " + count);
 		db.close();
 	});
+	// emails.find({date:"2000-11-13 10:06:00-08:00"}).toArray(function(err,docs){
+	// 	console.log(docs);
+	// 	db.close();
+	// });
 });
